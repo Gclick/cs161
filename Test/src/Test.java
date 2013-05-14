7428caeea567e7a0e3ab17a0e69a85ecbff0241d
kanwal <fauji@yahoo.com>
2013-05-14 13:52:09 -0700

    Initial checkin

---
+++ Test/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+	    System.out.println(3 + 25 + " some string.");
+	    System.out.println(" some string." + 3 + 25);
+	    System.out.println(" some string." + (3 + 25));
+	}
+}
